d15ce0098dea538832c99b542318b64a23f49f9d
Soksoa <153105972+Soksoa@users.noreply.github.com>
2024-06-18 17:52:04 +0200

    Camera permission functionality 🔒

--- app/(tabs)/camera.tsx
+++ app/(tabs)/camera.tsx
@@ -1,25 +1,46 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, Button } from "react-native";
+import { useState } from "react";
+import { useCameraPermissions } from "expo-camera";
 
 import EditScreenInfo from "../../components/EditScreenInfo";
 import { Text, View } from "../../components/Themed";
 
 export default function CameraTabScreen() {
-  return (
-    <View style={styles.container}>
-      <Text
-        style={styles.title}
-        className="text-purple-500 text-sm font-extrabold"
-      >
-        Camera
-      </Text>
-      <View
-        style={styles.separator}
-        lightColor="#eee"
-        darkColor="rgba(255,255,255,0.1)"
-      />
-      <EditScreenInfo path="app/(tabs)/two.tsx" />
-    </View>
-  );
+
+  const [facing, setFacing] = useState('back');
+  const [permission, requestPermission] = useCameraPermissions();
+
+  if (!permission) {
+    // Camera permissions are still loading.
+    return (
+      <View style={styles.container}>
+        <Text
+          style={styles.title}
+          className="text-purple-500 text-sm font-extrabold"
+        >
+          Camera
+        </Text>
+        <View
+          style={styles.separator}
+          lightColor="#eee"
+          darkColor="rgba(255,255,255,0.1)"
+        />
+        <EditScreenInfo path="app/(tabs)/two.tsx" />
+      </View>
+    );
+  }
+
+  if (!permission.granted) {
+    // Camera permissions are not granted yet.
+    return (
+      <View style={styles.container}>
+        <Text style={{ textAlign: 'center' }}>We need your permission to show the camera</Text>
+        <Button onPress={requestPermission} title="grant permission" />
+      </View>
+    );
+  }
+
+  
 }
 
 const styles = StyleSheet.create({
